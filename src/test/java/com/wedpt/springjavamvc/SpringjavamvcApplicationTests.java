ee0b492c458833d2ecf012281bd9104d4fff9f9c
phantunghoc <tung1982005@gmail.com>
2025-05-09 21:39:11 +0700

    first commmit

---
+++ src/test/java/com/wedpt/springjavamvc/SpringjavamvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wedpt.springjavamvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringjavamvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
