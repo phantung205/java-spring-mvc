e69f3ef4906f6bcd7151bfd959073373980c1ee7
phantunghoc <tung1982005@gmail.com>
2025-05-13 09:16:27 +0700

    update code two

--- src/main/java/com/wedpt/springjavamvc/SpringjavamvcApplication.java
+++ src/main/java/com/wedpt/springjavamvc/SpringjavamvcApplication.java
@@ -3,7 +3,9 @@ package com.wedpt.springjavamvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// include bao gom >< exclude loai bo
+@SpringBootApplication(exclude =
+org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class SpringjavamvcApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/wedpt/springjavamvc/service/MyService.java
@@ -0,0 +1,8 @@
+package com.wedpt.springjavamvc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyService {
+    
+}
