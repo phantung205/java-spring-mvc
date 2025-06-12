70cb629e9b63bb5bb4e08eb2f1733f92ffe12027
phantunghoc <tung1982005@gmail.com>
2025-06-12 16:17:24 +0700

    four commit

--- src/main/java/com/wedpt/springjavamvc/controller/UserController.java
+++ src/main/java/com/wedpt/springjavamvc/controller/UserController.java
@@ -1,11 +1,19 @@
 package com.wedpt.springjavamvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
+import com.wedpt.springjavamvc.domain.User;
 import com.wedpt.springjavamvc.service.UserService;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 @Controller
@@ -19,25 +27,22 @@ public class UserController {
 
 
     @RequestMapping("/")
-    public String getHomePage(){
+    public String getHomePage(Model  model ){
         String test = userService.handlHome();
+        model.addAttribute("phantungit",test);
+        model.addAttribute("name", "abc");
         return "hello";
     }
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model  model){
+        model.addAttribute("newUser",new User());
+        return "admin/user/create";
+    }
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model,@ModelAttribute("newUser") User phantungit){
+        System.out.println("run here"+phantungit);
+        return "hello";
+    }     
 
 }
-// @RestController
-// public class UserController {
 
-
-//     private UserService userService;
-
-//     public UserController(UserService userService) {
-//         this.userService = userService;
-//     }
-    
-
-//     @GetMapping("/")
-//     public String getHomePage(){
-//         return " this home page" ;
-//     }
-// }
